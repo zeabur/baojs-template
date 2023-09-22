cde2adea122a6dd1dbe9c734c6bbdb030ba69d13
michaelyuhe <2312744987@qq.com>
2023-09-22 14:32:28 +0800

    update Readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bao.js Template
+
+[Bao.js](https://github.com/mattreid1/baojs) is a fast, minimalist web framework for the Bun JavaScript runtime.
+
+This template is a simple Bao.js app.
+
+## How to Start
+
+```bash
+bun install
+bun run start
+```
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Bao from "baojs";
 const app = new Bao();
 
 app.get("/", (ctx) => {
-  return ctx.sendText("Hello World!");
+  return ctx.sendText("Hello, this is Bao.js from Zeabur!");
 });
 
 const server = app.listen({port: process.env.PORT || 3000});
